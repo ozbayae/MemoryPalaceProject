c1cbf5cac3a899425d762485bc24790f1478f23c
A.E. Özbay <ozbay.ae@gmail.com>
2023-03-07 12:42:57 +0100

    Bug fix, plus starting on UI

--- Assets/Scripts/FlashCard.cs
+++ Assets/Scripts/FlashCard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FlashCard : MonoBehaviour
@@ -8,8 +9,8 @@ public class FlashCard : MonoBehaviour
     private string _frontText = "This is placeholder text for the front of the card.";
     private string _backText = "This is placeholder text for the back of the card."; 
     
-    private TextMesh _frontTextMesh;
-    private TextMesh _backTextMesh;
+    public TextMeshPro _frontTextMesh;
+    public TextMeshPro _backTextMesh;
     
     private float _angle = 0; // This is at what angle ( 0 - 360 ) on the panorama the card is located
     private float _distance = 5; // This is how far away from the center of the panorama the card is located, in meters
@@ -20,8 +21,6 @@ public class FlashCard : MonoBehaviour
 // Start is called before the first frame update
     void Start()
     {
-        _frontTextMesh = transform.Find("Front").GetComponent<TextMesh>();
-        _backTextMesh = transform.Find("Back").GetComponent<TextMesh>();
         SetText(_frontText, _backText);
     }
 
